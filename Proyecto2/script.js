e3fbfa53954bf2712f3bec815a406faf05d9cd6c
Jhon-MVP <lesterbic@gmail.com>
2024-07-07 17:18:15 -0500

    Add files via upload
    Proyecto de investigación del segundo parcial Algoritmo y Lógica de Programación

---
+++ Proyecto2/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Aquí puedes agregar interactividad con JavaScript
+    console.log('Página cargada completamente');
+});
